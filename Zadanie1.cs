afe59b5e61bf62eec5379ba1099ebb9e4f554edd
kaczzzor <134389009+kaczzzor@users.noreply.github.com>
2023-05-25 13:05:53 +0200

    Add files via upload

---
+++ Zadanie1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string line;
+        string filePath = "C:\\test_Nikodem_Kaczorkowski.txt";
+
+        try
+        {
+            string text = File.ReadAllText(filePath);
+            StreamReader sr = new StreamReader("C:\\test_Nikodem_Kaczorkowski.txt");
+            line = sr.ReadLine();
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = sr.ReadLine();
+            }
+            sr.Close();
+            int count = counthowmany(text, 'a');
+            Console.WriteLine("Liczba wystąpień litery 'a': " + count);
+            Console.ReadLine();
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Plik nie został znaleziony.");
+        }
+        static int counthowmany(string text, char character)
+        {
+            int count = 0;
+            foreach (char c in text)
+            {
+                if (c == character)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
